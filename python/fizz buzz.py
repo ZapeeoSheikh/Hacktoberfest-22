57838d5cab166d32606673a043431d41890a6e32
XFarooqi <arfarooqix@gmail.com>
2022-10-02 13:41:15 +0500

    Added Fizz Buzz

---
+++ python/fizz buzz.py
@@ -0,0 +1,15 @@
+#Fizzbuzz Game
+
+def task(num):
+    for i in range(1,num):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+task(16)
+        
