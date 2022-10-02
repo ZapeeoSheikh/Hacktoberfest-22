c35491db140dc5f436dd1f787f64412f99c2d894
UditJain77 <uditjuice@gmail.com>
2022-10-02 22:17:42 +0530

    Added MultilevelInheritance.java

---
+++ Java/Inheritance/SingleInheritance.java
@@ -0,0 +1,12 @@
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void bark(){System.out.println("barking...");}  
+}  
+class SingleInheritance{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+d.bark();  
+d.eat();  
+}}  
