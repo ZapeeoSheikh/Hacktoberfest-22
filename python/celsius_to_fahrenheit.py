c86b13e090efda1cd7712ad7a1a0c83a2db56079
Paidipelly Dhruvateja <81409709+P-Dhruva-Teja@users.noreply.github.com>
2022-10-02 12:56:22 +0530

    Create celsius_to_fahrenheit.py

---
+++ python/celsius_to_fahrenheit.py
@@ -0,0 +1,3 @@
+celsius = float(input("Enter the temperature in celsius : "))
+fahrenheit = (celsius * 1.8) + 32
+print('{} degree Celsius is equal to {} degree Fahrenheit' .format(celsius,fahrenheit))
