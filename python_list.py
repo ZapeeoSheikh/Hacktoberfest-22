9a92e1a8363fbc46961fa77ccd4d417eee7ba70e
surajkankal <115442472+surajkankal@users.noreply.github.com>
2022-10-23 16:38:31 +0530

    Create python_list.py

---
+++ python_list.py
@@ -0,0 +1,10 @@
+num=[58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,76,74,75,76]
+print('Even Numbers are: ')
+for i in num:
+    if(i%2==0):
+        print(i)
+
+print('Odd Numbers are: ')
+for j in num:
+    if(j%2==1):
+        print(j)
